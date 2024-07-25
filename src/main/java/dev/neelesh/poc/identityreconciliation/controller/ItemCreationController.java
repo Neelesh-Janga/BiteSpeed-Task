1324f7c863e14586fde530bcbe8db430e6381b7d
Neelesh Janga <codelikeneelesh@gmail.com>
2024-07-25 16:58:42 +0530

    Added Controller with an end point for post requests

---
+++ src/main/java/dev/neelesh/poc/identityreconciliation/controller/ItemCreationController.java
@@ -0,0 +1,22 @@
+package dev.neelesh.poc.identityreconciliation.controller;
+
+import dev.neelesh.poc.identityreconciliation.dto.ConsolidatedContact;
+import dev.neelesh.poc.identityreconciliation.dto.Order;
+import dev.neelesh.poc.identityreconciliation.service.ContactService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class ItemCreationController {
+    
+    private final ContactService contactService;
+    
+    @PostMapping("/identify")
+    public ResponseEntity<ConsolidatedContact> identify(@RequestBody Order order) {
+        return contactService.getConsolidatedContactResponse(order.getEmail(), order.getPhoneNumber());
+    }
+}
