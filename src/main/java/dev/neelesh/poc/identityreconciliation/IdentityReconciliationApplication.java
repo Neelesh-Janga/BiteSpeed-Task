8c51ef8970c6d9cc669d2e713f1d96c0f8f4ffc7
Neelesh Janga <codelikeneelesh@gmail.com>
2024-07-25 12:47:51 +0530

    Created SpringBoot Application to run the PoC

---
+++ src/main/java/dev/neelesh/poc/identityreconciliation/IdentityReconciliationApplication.java
@@ -0,0 +1,12 @@
+package dev.neelesh.poc.identityreconciliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityReconciliationApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityReconciliationApplication.class, args);
+    }
+}
