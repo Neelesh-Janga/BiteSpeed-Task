57d46bd8b7f7868eff2544a968182f9aad646198
Neelesh Janga <codelikeneelesh@gmail.com>
2024-07-25 12:54:44 +0530

    Updated DateTimeFormatHelper

--- src/main/java/dev/neelesh/poc/identityreconciliation/util/DateTimeFormatHelper.java
+++ src/main/java/dev/neelesh/poc/identityreconciliation/util/DateTimeFormatHelper.java
@@ -1,26 +1,20 @@
 package dev.neelesh.poc.identityreconciliation.util;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Component
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class DateTimeFormatHelper {
     
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     
-    public LocalDateTime date() {
+    public static LocalDateTime date() {
         return LocalDateTime.parse(modifyTime(), formatter);
     }
     
-    private String modifyTime() {
+    private static String modifyTime() {
         LocalDateTime now = LocalDateTime.now();
         return now.format(formatter);
     }
